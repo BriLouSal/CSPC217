58dac93890f7fd0156a5655f1f681908f3d0325a
Brian Louis <brianlouis.salinas116@gmail.com>
2025-09-23 19:37:34 -0600

    Notes!

---
+++ lec9.py
@@ -0,0 +1,41 @@
+# :oops
+# Lec 9
+
+
+# While loops, for loops
+
+
+i = 1
+
+
+while i <= 3:
+    print(i)
+    i += 1
+    print(i)
+#
+
+
+
+age = int(input("Enter your age:  "))
+
+birthyear = int(input("When were you born: "))
+
+while age != 100:
+    birthyear += 1
+    age += 1
+
+
+
+print(f"The year when you'll be a 100 is {birthyear}")
+
+
+
+
+runner = 3
+while runner >= 1:
+    print("Runner=", runner)
+
+print(runner)
+
+
+# We also learnt truth tables!
