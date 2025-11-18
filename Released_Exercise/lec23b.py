3594358c1802be81223f3cc4db99b4628cc597ab
Brian Louis <brianlouis.salinas116@gmail.com>
2025-11-18 15:45:16 -0700

    Added new files

---
+++ Released_Exercise/lec23b.py
@@ -0,0 +1,9 @@
+matrix = [[0] * 3] * 2
+print(matrix)
+
+# It would check the digit as if you try it with a string, it would automatically ignore the \n such as
+# that
+
+
+# The way you can remove via adding the words into the list (which would include the iteration which I think so
+# and then remove it into a
